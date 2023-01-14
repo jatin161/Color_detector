f6deb8e463b3ad8693dc5f57166fa1fd4e92955b
jatin161 <35172881+jatin161@users.noreply.github.com>
2023-01-14 23:43:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,8 @@ with col1:
 with col2:
     if st.button("Get Colors Loaded"):
         # st.text("An Alternate Widow is opened. Press Esc to close that window")
-        import cv2
+        from cv2 import imshow,LINE_AA,imread,EVENT_LBUTTONDBLCLK,namedWindow,WINDOW_NORMAL,resizeWindow,setMouseCallback,rectangle,putText,waitKey,destroyAllWindows
+
         import pandas as pd
 
         if option == 'Sample-1':
@@ -87,7 +88,7 @@ with col2:
             img_path = r"sample2.jpg"
         if option == 'Sample-3':
             img_path = r"Sample3.jpg"
-        img = cv2.imread(img_path)
+        img = imread(img_path)
         clicked = False
         r = g = b = x_pos = y_pos = 0
         csv = pd.DataFrame(color)
@@ -106,7 +107,7 @@ with col2:
 
         # function to get x,y coordinates of mouse double click
         def draw_function(event, x, y, flags, param):
-            if event == cv2.EVENT_LBUTTONDBLCLK:
+            if event == EVENT_LBUTTONDBLCLK:
                 global b, g, r, x_pos, y_pos, clicked
                 clicked = True
                 x_pos = x
@@ -117,34 +118,35 @@ with col2:
                 r = int(r)
 
 
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('image', 600, 800)
-        cv2.setMouseCallback('image', draw_function)
+        namedWindow('image', WINDOW_NORMAL)
+        resizeWindow('image', 600, 800)
+        setMouseCallback('image', draw_function)
+
         while True:
 
-            cv2.imshow("image", img)
+            imshow("image", img)
             if clicked:
 
                 # cv2.rectangle(image, start point, endpoint, color, thickness)-1 fills entire rectangle
-                cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+                rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
                 # Creating text string to display( Color name and RGB values )
                 text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
 
                 # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-                cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+                putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, LINE_AA)
 
                 # For very light colours we will display text in black colour
                 if r + g + b >= 600:
-                    cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+                    putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, LINE_AA)
 
                 clicked = False
 
             # Break the loop when user hits 'esc' key
-            if cv2.waitKey(20) & 0xFF == 27:
+            if waitKey(20) & 0xFF == 27:
                 break
 
-        cv2.destroyAllWindows()
+        destroyAllWindows()
 with col3:
     st.write(
         """
